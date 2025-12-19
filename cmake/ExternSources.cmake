297a2ae0dd96dba5501428d6c6df41d8bc956a30
pbutti <pierfrancesco.butti@gmail.com>
2025-12-19 21:28:27 +0100

    Add cmake, pybind and gitignore

---
+++ cmake/ExternSources.cmake
@@ -0,0 +1,4 @@
+set(ACTS_PYBIND11_SOURCE
+    "GIT_REPOSITORY;https://github.com/pybind/pybind11.git;GIT_TAG;v${_acts_pybind11_version}"
+)
+mark_as_advanced(ACTS_PYBIND11_SOURCE)
--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ export MAIN_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd
 
 # Add the directory to the python path
 export PYTHONPATH="${MAIN_DIR}:${PYTHONPATH}"
+
+# Add the local python bindings to the python path
+export PYTHONPATH="${MAIN_DIR}/build/ActsAlgorithms/python:${PYTHONPATH}"
